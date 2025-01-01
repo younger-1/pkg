25a867a2ba0cf7bd3b293a6125df22c72797616d
Xavier Young <45989017+younger-1@users.noreply.github.com>
2025-01-01 12:33:02 +0800

    go: move file location
    Change-Id: Ic9e758ad3026d6b575348770d45bc9477d7af4ad

--- go/go.mod
+++ go/go.mod
@@ -1,4 +1,4 @@
-module github.com/my/repo
+module github.com/younger-1/code-playground/go
 
 go 1.23.1
 
--- go/hello.go
+++ go/hello.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 
 	"github.com/google/go-cmp/cmp"
-	"github.com/my/repo/morestrings"
+	"github.com/younger-1/code-playground/go/morestrings"
 )
 
 func main() {
